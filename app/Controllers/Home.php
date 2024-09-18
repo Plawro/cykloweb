f05ffdeddeb4b1b15510d668ab47f3363353b381
Aleš Hanáček <ales.hanacekit@seznam.cz>
2024-09-18 12:25:24 +0200

    Saved files for fix

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,48 @@
 
 namespace App\Controllers;
 
+use App\Controllers\BaseController;
+use App\Models\Race;
+
 class Home extends BaseController
 {
+    var $race;
+    var $raceyear;
+
+    public function __construct()
+    {
+        $this->race = new Race();
+    }
+
     public function index(): string
     {
         return view('home');
+    }
+
+    public function teams(): string
+    {
+        return view('teams');
+    }
+
+    public function races(): string
+    {
+        $data['race'] = $this->race->paginate(25);
+        $data['pager'] = $this->race->pager;
+        return view('races', $data);
+    }
+
+    public function race(): string
+    {
+        return view('race');
+    }
+
+    public function team(): string
+    {
+        return view('team');
+    }
+
+    public function admin(): string
+    {
+        return view('admin');
     }
 }
